df7b86d9110eb2063c6627cac68246348545e94f
Graham Hukill <ghukill@gmail.com>
2016-08-28 15:44:32 -0400

    first commie

---
+++ oaiglow/views.py
@@ -0,0 +1,17 @@
+# views for oaiglow
+
+# flask proper
+from flask import render_template, request, session, redirect, make_response, Response
+
+# oaiglow flask app
+from oaiglow import oaiglow_app
+
+# localConfig
+import localConfig
+
+
+@oaiglow_app.route("/%s/" % (localConfig.OAIGLOW_APP_PREFIX), methods=['POST', 'GET'])
+@oaiglow_app.route("/%s" % (localConfig.OAIGLOW_APP_PREFIX), methods=['POST', 'GET'])
+def index():	
+
+	return "Hello oaiglow world."
---
+++ runserver.py
@@ -0,0 +1,32 @@
+
+# twisted imports
+from twisted.web.wsgi import WSGIResource
+from twisted.web.server import Site
+from twisted.internet import reactor, defer
+from twisted.internet.task import deferLater
+from twisted.web.server import NOT_DONE_YET
+from twisted.web import server, resource
+from twisted.python import log
+
+# oaiglow imports
+from oaiglow import oaiglow_app
+
+# localConfig
+import localConfig
+
+# general imports
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# WSUDOR_API_app
+oaiglow_resource = WSGIResource(reactor, reactor.getThreadPool(), oaiglow_app)
+oaiglow_site = Site(oaiglow_resource)
+
+if __name__ == '__main__':
+
+	# main oaiglow app
+	logging.debug("starting oaiglow app at :%d, /%s..." % (localConfig.OAIGLOW_APP_PORT, localConfig.OAIGLOW_APP_PREFIX))
+	reactor.listenTCP( localConfig.OAIGLOW_APP_PORT, oaiglow_site )
+
+	logging.debug('oaiglow started')
+	reactor.run()
